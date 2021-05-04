d49aad84d990d777788e2edc35e27b7d9a0541ff
Samuel de Brito Santos <80654468+coder-samuel@users.noreply.github.com>
2021-05-03 22:02:31 -0300

    Add files via upload

---
+++ mentalidades/mentalidades/src/main/java/com/mentalidades/mentalidades/controller/MentalidadesController.java
@@ -0,0 +1,16 @@
+package com.mentalidades.mentalidades.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+	@RestController
+	@RequestMapping ("/mentalidades")
+	public class MentalidadesController {
+		
+			@GetMapping
+			public String mentalidades() {
+				return "Mentalidades Generation:\nPersistência\nResponsabilidade Pessoal";
+				
+			}
+}
